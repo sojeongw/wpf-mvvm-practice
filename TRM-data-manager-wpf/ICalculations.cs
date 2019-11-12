f51d1e1145dadad7ca786ca3d293cb06e1a88c52
sojeongw <redthink90@gmail.com>
2019-11-12 16:20:09 +0900

    Test Dependency Injection with Calculations.cs

---
+++ TRM-data-manager-wpf/ICalculations.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace TRM_data_manager_wpf
+{
+    public interface ICalculations
+    {
+        List<string> Register { get; set; }
+
+        double Add(double x, double y);
+    }
+}
--- TRM-data-manager-wpf/ViewModels/ShellViewModel.cs
+++ TRM-data-manager-wpf/ViewModels/ShellViewModel.cs
@@ -8,5 +8,10 @@ namespace TRM_data_manager_wpf.ViewModels
 {
     public class ShellViewModel
     {
+        private ICalculations _calculations;
+        public ShellViewModel(ICalculations calculations)
+        {
+            _calculations = calculations;
+        }
     }
 }
